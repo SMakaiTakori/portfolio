4fa55986f6e2353d7fe81f89f574a2eae5998aee
SMakaiTakori <makait87@icloud.com>
2020-11-09 18:43:57 -0500

    connected react-bootstrap

--- portfolio-website/src/components/App.js
+++ portfolio-website/src/components/App.js
@@ -1,7 +1,5 @@
 import React from 'react'
-
-
-
+import Alert from 'react-bootstrap/Alert'
 
 
 
@@ -12,6 +10,13 @@ import React from 'react'
 
            <h1> S. Makai Takori</h1> 
             <br/>
+
+            <Alert variant="success">
+                This is a success alert—check it out!
+            </Alert>
+
+
+
            <h1>About Makai</h1>
            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             <br/>
