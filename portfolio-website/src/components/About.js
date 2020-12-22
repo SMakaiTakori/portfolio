b1e622dfd1c8e87d5e5692e9c7c6713f1e36cf1d
SMakaiTakori <makait87@icloud.com>
2020-12-21 21:59:31 -0500

    setup projects container

--- portfolio-website/src/components/About.js
+++ portfolio-website/src/components/About.js
@@ -1,32 +1,40 @@
-import React from 'react'
+import React from "react";
+import ReactPlayer from "react-player";
 
-import Profile from '../images/Headshot.jpg'
+import Profile from "../images/Headshot.jpg";
 
-import Container from 'react-bootstrap/Container'
-import Col from 'react-bootstrap/Col'
-import Row from 'react-bootstrap/Row'
-import Image from 'react-bootstrap/Image'
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Image from "react-bootstrap/Image";
 
-
- const About = () => {
-    return (
-        <div>
-           
-            <br/>
-             <Container>
-                <Row className= "rows">
-                    <Col> 
-                        <Image  src= {Profile} roundedCircle /> 
-                    </Col>
-                    <Col> 
-                    <br/>
-                       <h1> About Makai </h1>
-                    <br/>                    
-                       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>                            </Col>
-                </Row>
-            </Container>
-        </div>
-    )
-}
+const About = () => {
+  return (
+    <div>
+      <br />
+      <Container>
+        <Row className="rows">
+          <Col>
+            <Image src={Profile} roundedCircle />
+          </Col>
+          <Col>
+            <br />
+            <h1> About Makai </h1>
+            <br />
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.{" "}
+            </p>{" "}
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
 
 export default About;
--- portfolio-website/src/components/App.js
+++ portfolio-website/src/components/App.js
@@ -1,57 +1,50 @@
-import React from 'react'
-import Alert from 'react-bootstrap/Alert'
-import About from './About'
-import Image from 'react-bootstrap/Image'
+import React from "react";
+// import Alert from 'react-bootstrap/Alert'
+import About from "./About";
+import Image from "react-bootstrap/Image";
 
-import Background from '../images/Woodbackground.jpg'
+import Background from "../images/Woodbackground.jpg";
 
-
-
- const App = () => {
-
-    return (
-        <div>
-            <Image style= {{width:2000, height:500 }} src={Background}/>  
-          
-            <About/>
-            
-           <h1>Personal Projects</h1>
-           <table>
-               <tr>
-                   
-                    <label>Image 1</label> <br/>
-                    <label> Project title| Link</label>
-                    <p>Project description</p>
-                </tr>
-                <tr>
-                    <label>Image 2 </label><br/>
-                    <label>Project title| Link</label>
-                    <p>Project description</p>
-                </tr>
-                <tr>
-                         <label>Image 3</label><br/>
-                    <label>Project title| Link</label>
-                    <p>Project description</p>
-                </tr>
-            </table>
-            <br/>
-                <h1> Client Projects </h1>
-                
-                <p>Capstone Consulting</p>
-                <p>Advantage Consulting</p>
-                <p>National Black Arts Festival</p>
-                <p>Twice A Child</p>
-               
-            <br/>
-                <h1>Blog</h1>
-                <label>Read my latest blog here</label>
-            <br/>
-           <h1>Contact</h1>
-                You can contact me at ...
-                Or follow me on social media
-            <br/>
-        </div>
-    )
-}
+const App = () => {
+  return (
+    <div>
+      <Image style={{ width: 2000, height: 500 }} src={Background} />
+      <About />
+      <h1>Personal Projects</h1>
+      <table>
+        <tr>
+          <label>Image 1</label> <br />
+          <label> Project title| Link</label>
+          <p>Project description</p>
+        </tr>
+        <tr>
+          <label>Image 2 </label>
+          <br />
+          <label>Project title| Link</label>
+          <p>Project description</p>
+        </tr>
+        <tr>
+          <label>Image 3</label>
+          <br />
+          <label>Project title| Link</label>
+          <p>Project description</p>
+        </tr>
+      </table>
+      <br />
+      <h1> Client Projects </h1>
+      <p>Capstone Consulting</p>
+      <p>Advantage Consulting</p>
+      <p>National Black Arts Festival</p>
+      <p>Twice A Child</p>
+      <br />
+      <h1>Blog</h1>
+      <label>Read my latest blog here</label>
+      <br />
+      <h1>Contact</h1>
+      You can contact me at ... Or follow me on social media
+      <br />
+    </div>
+  );
+};
 
 export default App;
