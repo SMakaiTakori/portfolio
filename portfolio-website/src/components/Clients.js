d7347c1836e21626d738bb7490e14e50f972f2c5
SMakaiTakori <makait87@icloud.com>
2020-12-25 21:07:33 -0500

    added two images to carousel

--- portfolio-website/src/components/Clients.js
+++ portfolio-website/src/components/Clients.js
@@ -1,6 +1,8 @@
 import React from "react";
 
 import Carousel from "react-bootstrap/Carousel";
+import TwiceAChild from "../images/TwiceAChild.png";
+import NBAF from "../images/NBAF1.png";
 
 const Clients = () => {
   return (
@@ -11,33 +13,29 @@ const Clients = () => {
         <Carousel.Item>
           <img
             className="d-block w-100"
-            src="holder.js/800x400?text=First slide&bg=373940"
-            alt="First slide"
+            src={TwiceAChild}
+            alt="Twice A Child"
           />
           <Carousel.Caption>
-            <h3>First slide label</h3>
+            <h3>Twice A Child</h3>
             <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
           </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=Second slide&bg=282c34"
-            alt="Third slide"
-          />
 
+        <Carousel.Item>
+          <img className="d-block w-100" src={NBAF} alt="NBAF" />
           <Carousel.Caption>
-            <h3>Second slide label</h3>
+            <h3>National Black Arts Festival</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
           </Carousel.Caption>
         </Carousel.Item>
+
         <Carousel.Item>
           <img
             className="d-block w-100"
             src="holder.js/800x400?text=Third slide&bg=20232a"
             alt="Third slide"
           />
-
           <Carousel.Caption>
             <h3>Third slide label</h3>
             <p>
