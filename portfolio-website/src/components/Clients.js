9e6dd1b70a62b7c4b3b15f704453aea1b6a036d1
SMakaiTakori <makait87@icloud.com>
2020-12-25 21:48:02 -0500

    made carousel images links to corresponding websites

--- portfolio-website/src/components/Clients.js
+++ portfolio-website/src/components/Clients.js
@@ -3,7 +3,7 @@ import React from "react";
 import Carousel from "react-bootstrap/Carousel";
 
 import TwiceAChild from "../images/TwiceAChild2.png";
-import NBAF from "../images/NBAF2.png";
+import NBAF from "../images/NBAF3.png";
 import Capstone from "../images/Capstone.png";
 import Advantage from "../images/AdvantageConsulting.png";
 
@@ -14,19 +14,23 @@ const Clients = () => {
       <br />
       <hr />
       <Carousel>
-        <Carousel.Item interval={2000} height="200px">
-          <img className="d-block w-100" src={NBAF} alt="NBAF" />
+        <Carousel.Item interval={4000}>
+          <a href="http://nbaf.org/" target="_blank">
+            <img className="d-block w-100" src={NBAF} alt="NBAF" />
+          </a>
           <Carousel.Caption>
             {/* <h3>National Black Arts Festival</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
           </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item interval={2000}>
-          <img
-            className="d-block w-100"
-            src={Advantage}
-            alt="Advantage Consulting"
-          />
+        <Carousel.Item interval={4000}>
+          <a href="https://advantageconsultingllc.com/" target="_blank">
+            <img
+              className="d-block w-100"
+              src={Advantage}
+              alt="Advantage Consulting"
+            />
+          </a>
           <Carousel.Caption>
             {/* <h3>Advantage Consulting LLC</h3>
             <p>
@@ -35,23 +39,27 @@ const Clients = () => {
           </Carousel.Caption>
         </Carousel.Item>
 
-        <Carousel.Item interval={2000}>
-          <img
-            className="d-block w-100"
-            src={TwiceAChild}
-            alt="Twice A Child"
-          />
+        <Carousel.Item interval={4000}>
+          <a href="https://twiceachildpodcast.com/" target="_blank">
+            <img
+              className="d-block w-100"
+              src={TwiceAChild}
+              alt="Twice A Child"
+            />
+          </a>
           <Carousel.Caption>
             {/* <h3>Twice A Child</h3>
             <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
           </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item interval={2000}>
-          <img
-            className="d-block w-100"
-            src={Capstone}
-            alt="Capstone Alternative Strategies"
-          />
+        <Carousel.Item interval={4000}>
+          <a href="https://www.capaltstrategies.com/" target="_blank">
+            <img
+              className="d-block w-100"
+              src={Capstone}
+              alt="Capstone Alternative Strategies"
+            />
+          </a>
           <Carousel.Caption>
             {/* <h3>Capstone Alternative Strategies</h3>
             <p>
