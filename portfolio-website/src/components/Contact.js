96b314ef039abed5c5aa494bcdfb58d6e691850d
SMakaiTakori <makait87@icloud.com>
2020-12-30 22:24:56 -0500

    added email and github social icons

--- portfolio-website/src/components/Contact.js
+++ portfolio-website/src/components/Contact.js
@@ -1,13 +1,30 @@
 import React from "react";
 
 import { SocialIcon } from "react-social-icons";
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 
 const Contact = () => {
   return (
     <div>
       <h1 className="section-header">Contact</h1>
-      <SocialIcon url="http://twitter.com/MakaiTakori" />
-      <SocialIcon url="https://www.linkedin.com/in/makai-takori/" />
+      <Container>
+        <Row>
+          <Col>
+            <SocialIcon url="http://twitter.com/MakaiTakori" />
+          </Col>
+          <Col>
+            <SocialIcon url="https://www.linkedin.com/in/makai-takori/" />
+          </Col>
+          <Col>
+            <SocialIcon url="https://github.com/SMakaiTakori" />
+          </Col>
+          <Col>
+            <SocialIcon url="mailto:makai.takori@gmail.com" />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
