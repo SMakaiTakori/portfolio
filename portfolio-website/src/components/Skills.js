250f4c2426f9890d09d826d31eb50638ebb84c89
SMakaiTakori <makait87@icloud.com>
2021-01-11 21:38:26 -0500

    moved resume button

--- portfolio-website/src/components/Skills.js
+++ portfolio-website/src/components/Skills.js
@@ -1,13 +1,30 @@
 import React from "react";
+
 import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Image from "react-bootstrap/Image";
 
 const Skills = () => {
   return (
     <div>
       <h1 className="section-header">Skills</h1>
-      <a href="#">
-        <Button variant="primary">Download Resume</Button>
-      </a>
+
+      <Container className="">
+        <Row>
+          <Col className="">
+            {/* <Image className="" src={NBAF} /> */}
+            {/* <Image className="" src={TwiceAChild} /> */}
+
+            {/* <Image className="" src={Advantage} /> */}
+            {/* <Image className="" src={Capstone} /> */}
+            <a href="#">
+              <Button variant="primary">Download Resume</Button>
+            </a>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
