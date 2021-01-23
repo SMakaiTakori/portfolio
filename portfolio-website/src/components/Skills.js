8e1101512a82bddc8642e5a55f80b153c779a262
SMakaiTakori <makait87@icloud.com>
2021-01-23 17:58:55 -0500

    added more skills images, added to the DOM

--- portfolio-website/src/components/Skills.js
+++ portfolio-website/src/components/Skills.js
@@ -6,25 +6,35 @@ import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Image from "react-bootstrap/Image";
 
+import Rct from "../images/React-icon.png";
+import Rails from "../images/Ruby.png";
+import JS from "../images/javascript.jpg";
+import CSS from "../images/htmlcss.png";
+import Git from "../images/git.png";
+
 const Skills = () => {
   return (
     <div>
       <h1 className="section-header">Skills</h1>
+      <div className="skills-section">
+        <Container className="skills-container">
+          <Row>
+            <Col className="skills-icons">
+              <Image className="skills-list" src={Rct} />
+              <Image className="skills-list" src={Rails} />
 
-      <Container className="">
-        <Row>
-          <Col className="">
-            {/* <Image className="" src={NBAF} /> */}
-            {/* <Image className="" src={TwiceAChild} /> */}
-
-            {/* <Image className="" src={Advantage} /> */}
-            {/* <Image className="" src={Capstone} /> */}
+              <Image className="skills-list" src={JS} />
+              <Image className="skills-list" src={CSS} />
+              <Image className="skills-list" src={Git} />
+            </Col>
+          </Row>
+          {/* <Col className="">
             <a href="#">
-              <Button variant="primary">Download Resume</Button>
+              <Button className="skills-btn">View My Resume</Button>
             </a>
-          </Col>
-        </Row>
-      </Container>
+          </Col> */}
+        </Container>
+      </div>
     </div>
   );
 };
